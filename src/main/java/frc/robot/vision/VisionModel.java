97c5d668f6dc9440ba22f4ec54f8b743164e76e6
Caleb Denio <cjdenio44@gmail.com>
2022-03-12 13:04:43 -0500

    ✨ Vision

--- src/main/java/frc/robot/vision/VisionModel.java
+++ src/main/java/frc/robot/vision/VisionModel.java
@@ -3,7 +3,23 @@ package frc.robot.vision;
 import org.opencv.core.Scalar;
 
 public interface VisionModel {
-    public Scalar getLowerBound();
+    Scalar getLowerBound();
 
-    public Scalar getUpperBound();
+    Scalar getUpperBound();
+
+    default Double minArea() {
+        return null;
+    }
+
+    default Double maxArea() {
+        return null;
+    }
+
+    default boolean isRatioCorrect(double ratio) {
+        return true;
+    }
+
+    default boolean isPositionCorrect(double x, double y) {
+        return true;
+    }
 }
